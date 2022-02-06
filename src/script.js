c42e701d4e8c0594e859ea0d04f1e3bf4f20640b
Anwar M. Abrar <88562556+anushady@users.noreply.github.com>
2022-02-06 21:47:24 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -2,14 +2,15 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { AmbientLight } from "three";
+import { AmbientLight, Mesh, WireframeGeometry } from "three";
+import { OutlineEffect } from "three/examples/jsm/effects/OutlineEffect.js";
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
-
+let effect;
 // Scene
 const scene = new THREE.Scene();
-
+//scene.background = new THREE.Color(0x444488);
 // var loader = new GLTFLoader();
 // var obj1;
 // loader.load(
@@ -26,36 +27,56 @@ const scene = new THREE.Scene();
 // );
 // Lights
 
-const obj = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial(0xffffff);
-const obj1 = new THREE.Mesh(obj, material);
-obj1.position.set(3, 0, 0);
+// const obj = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshToonMaterial({
+//   color: 0xff0000,
 
-scene.add(obj1);
+//   //emmisive: 0x000000,
+//   //vertexColors: true,
+//   //side: THREE.FrontSide,
+//   //shininess: 100,
+//   //specular: 0xffffff,
+// });
+// const obj1 = new THREE.Mesh(obj, material);
+// obj1.position.set(1.3, 0, 0);
+// obj1.rotation.set(Math.PI / 8, 0, 0);
 
-const pointLight = new THREE.PointLight(0xffffff, 0.3);
-pointLight.position.x = 2;
-pointLight.position.y = -3;
-pointLight.position.z = -4;
-scene.add(pointLight);
-const pointLight2 = new THREE.PointLight(0xffffff, 0.3);
-pointLight2.position.x = -2;
-pointLight2.position.y = -3;
-pointLight2.position.z = 4;
-scene.add(pointLight2);
-const pointLight3 = new THREE.PointLight(0xffffff, 0.3);
-pointLight3.position.x = 2;
-pointLight3.position.y = 3;
-pointLight3.position.z = -4;
-scene.add(pointLight3);
-const pointLight4 = new THREE.PointLight(0xffffff, 0.3);
-pointLight4.position.x = -2;
-pointLight4.position.y = 3;
-pointLight4.position.z = 4;
-scene.add(pointLight4);
+// scene.add(obj1);
 
-const ambLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambLight);
+// const pointLight = new THREE.PointLight(0xffffff, 0.5);
+// pointLight.position.x = 2;
+// pointLight.position.y = -3;
+// pointLight.position.z = -4;
+// scene.add(pointLight);
+// const pointLight2 = new THREE.PointLight(0xffffff, 0.6);
+// pointLight2.position.x = -2;
+// pointLight2.position.y = -3;
+// pointLight2.position.z = 4;
+// scene.add(pointLight2);
+// const pointLight3 = new THREE.PointLight(0xffffff, 0.5);
+// pointLight3.position.x = 2;
+// pointLight3.position.y = 3;
+// pointLight3.position.z = -4;
+// scene.add(pointLight3);
+// const pointLight4 = new THREE.PointLight(0xffffff, 0.5);
+// pointLight4.position.x = -2;
+// pointLight4.position.y = 3;
+// pointLight4.position.z = 4;
+// scene.add(pointLight4);
+
+// const ambLight = new THREE.AmbientLight(0xffffff, 0.5);
+// scene.add(ambLight);
+
+scene.add(new THREE.AmbientLight(0x444444));
+
+const light1 = new THREE.DirectionalLight(0xffffff, 0.5);
+light1.position.set(1, 1, 1);
+scene.add(light1);
+
+const light2 = new THREE.DirectionalLight(0xffffff, 1.5);
+light2.position.set(0, -1, 0);
+scene.add(light2);
+
 /**
  * Sizes
  */
@@ -83,19 +104,20 @@ window.addEventListener("resize", () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-  45,
+  75,
   sizes.width / sizes.height,
   0.1,
-  100
+  10000
 );
 camera.position.x = 0;
 camera.position.y = 0;
-camera.position.z = 4;
+camera.position.z = 1300;
+//camera.lookAt(0);
 scene.add(camera);
 
 // Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+//const controls = new OrbitControls(camera, canvas);
+//controls.enableDamping = true;
 
 //play ground
 
@@ -113,13 +135,12 @@ animationScripts.push({
   start: 0,
   end: 20,
   func: function () {
-    camera.lookAt(obj1.position);
-    camera.position.set(0, 0, 4);
     //camera.lookAt(obj1.position);
-    //obj1.position.set(2, 0, 0);
-    //obj1.position.x = lerp(0, 0, scalePercent(0, 20));
-    obj1.rotation.y = lerp(0, Math.PI, scalePercent(0, 20));
-    console.log(obj1.position.z);
+    mesh.position.x = lerp(900, 0, scalePercent(0, 20));
+    //material.color = 0xff0000;
+    //camera.position.set(0, 1, 2);
+    //obj1.position.z = lerp(-5, 0, scalePercent(0, 40));
+    //console.log(cube.position.z)
   },
 });
 
@@ -128,10 +149,10 @@ animationScripts.push({
   start: 20,
   end: 100,
   func: function () {
-    camera.lookAt(obj1.position);
-    camera.position.set(0, 0, 4);
-
-    obj1.rotation.y = lerp(0, 4 * Math.PI, scalePercent(20, 100));
+    material.color.set(0xffffff);
+    //camera.lookAt(obj1.position);
+    //camera.position.set(0, 1, 2);
+    mesh.rotation.y = lerp(0, 4 * Math.PI, scalePercent(20, 100));
     //console.log(obj1.rotation.z)
   },
 });
@@ -165,15 +186,201 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.outputEncoding = THREE.sRGBEncoding;
+
+/**
+ * Animate
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+const triangles = 300000;
+
+const geometry = new THREE.BufferGeometry();
+
+const positions = [];
+const normals = [];
+const colors = [];
+
+const color = new THREE.Color();
+
+const n = 800,
+  n2 = n / 2; // triangles spread in the cube
+const d = 12,
+  d2 = d / 2; // individual triangle size
+
+const pA = new THREE.Vector3();
+const pB = new THREE.Vector3();
+const pC = new THREE.Vector3();
+
+const cb = new THREE.Vector3();
+const ab = new THREE.Vector3();
+
+for (let i = 0; i < triangles; i++) {
+  // positions
+
+  const x = Math.random() * n - n2;
+  const y = Math.random() * n - n2;
+  const z = Math.random() * n - n2;
+
+  const ax = x + Math.random() * d - d2;
+  const ay = y + Math.random() * d - d2;
+  const az = z + Math.random() * d - d2;
+
+  const bx = x + Math.random() * d - d2;
+  const by = y + Math.random() * d - d2;
+  const bz = z + Math.random() * d - d2;
+
+  const cx = x + Math.random() * d - d2;
+  const cy = y + Math.random() * d - d2;
+  const cz = z + Math.random() * d - d2;
+
+  positions.push(ax, ay, az);
+  positions.push(bx, by, bz);
+  positions.push(cx, cy, cz);
+
+  // flat face normals
+
+  pA.set(ax, ay, az);
+  pB.set(bx, by, bz);
+  pC.set(cx, cy, cz);
+
+  cb.subVectors(pC, pB);
+  ab.subVectors(pA, pB);
+  cb.cross(ab);
+
+  cb.normalize();
+
+  const nx = cb.x;
+  const ny = cb.y;
+  const nz = cb.z;
+
+  normals.push(nx * 32767, ny * 32767, nz * 32767);
+  normals.push(nx * 32767, ny * 32767, nz * 32767);
+  normals.push(nx * 32767, ny * 32767, nz * 32767);
+
+  // colors
+
+  const vx = x / n + 0.5;
+  const vy = y / n + 0.5;
+  const vz = z / n + 0.5;
+
+  color.setRGB(vx, vy, vz);
+
+  colors.push(color.r * 255, color.g * 255, color.b * 255);
+  colors.push(color.r * 255, color.g * 255, color.b * 255);
+  colors.push(color.r * 255, color.g * 255, color.b * 255);
+}
+
+const positionAttribute = new THREE.Float32BufferAttribute(positions, 3);
+const normalAttribute = new THREE.Int16BufferAttribute(normals, 3);
+const colorAttribute = new THREE.Uint8BufferAttribute(colors, 3);
+
+normalAttribute.normalized = true; // this will map the buffer values to 0.0f - +1.0f in the shader
+colorAttribute.normalized = true;
+
+geometry.setAttribute("position", positionAttribute);
+geometry.setAttribute("normal", normalAttribute);
+geometry.setAttribute("color", colorAttribute);
+
+geometry.computeBoundingSphere();
+
+const material = new THREE.MeshPhongMaterial({
+  color: 0x000000,
+  specular: 0xffffff,
+  shininess: 250,
+  side: THREE.DoubleSide,
+  vertexColors: false,
+});
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// const cubeWidth = 400;
+// const numberOfSphersPerSide = 5;
+// const sphereRadius = (cubeWidth / numberOfSphersPerSide) * 1.7 * 0.5;
+// const stepSize = 1.0 / numberOfSphersPerSide;
+// const format = renderer.capabilities.isWebGL2
+//   ? THREE.RedFormat
+//   : THREE.LuminanceFormat;
+
+// const geometry = new THREE.BoxGeometry(
+//   sphereRadius,
+//   sphereRadius,
+//   sphereRadius
+// );
+// var colors;
+// for (
+//   let alpha = 0, alphaIndex = 0;
+//   alpha <= 1.0;
+//   alpha += stepSize, alphaIndex++
+// ) {
+//   colors = new Uint8Array(alphaIndex + 2);
+// }
+// const gradientMap = new THREE.DataTexture(colors, colors.length, 1, format);
+// gradientMap.needsUpdate = true;
+
+// const material = new THREE.MeshToonMaterial({
+//   color: "blue",
+//   gradientMap: gradientMap,
+// });
+
+// var mesh = new THREE.Mesh(geometry, material);
+
+// mesh.position.x = 400 - 200;
+// mesh.position.y = 400 - 200;
+// mesh.position.z = 400 - 200;
+// scene.add(mesh);
+
+// for (
+//   let alpha = 0, alphaIndex = 0;
+//   alpha <= 1.0;
+//   alpha += stepSize, alphaIndex++
+// ) {
+//   const colors = new Uint8Array(alphaIndex + 2);
+
+//   for (let c = 0; c <= colors.length; c++) {
+//     colors[c] = (c / colors.length) * 256;
+//   }
+
+//   const gradientMap = new THREE.DataTexture(colors, colors.length, 1, format);
+//   gradientMap.needsUpdate = true;
+//   var mesh;
+//   for (let beta = 0; beta <= 1.0; beta += stepSize) {
+//     for (let gamma = 0; gamma <= 1.0; gamma += stepSize) {
+//       // basic monochromatic energy preservation
+//       const diffuseColor = new THREE.Color()
+//         .setHSL(alpha, 0.5, gamma * 0.5 + 0.1)
+//         .multiplyScalar(1 - beta * 0.2);
+
+//       const material = new THREE.MeshToonMaterial({
+//         color: diffuseColor,
+//         gradientMap: gradientMap,
+//       });
+
+//       var mesh = new THREE.Mesh(geometry, material);
+
+//       mesh.position.x = alpha * 400 - 200;
+//       mesh.position.y = beta * 400 - 200;
+//       mesh.position.z = gamma * 400 - 200;
+//       scene.add(mesh);
+//     }
+//   }
+// }
+
+effect = new OutlineEffect(renderer);
+
 // const updateOnScroll = (event) => {
-//   obj1.rotation.y = window.scrollY * 0.00755;
+//   mesh.rotation.y = window.scrollY * 0.01;
 
 //   //obj12.position.z = window.scrollY *.001
 // };
 // window.addEventListener("scroll", updateOnScroll);
-/**
- * Animate
- */
 
 const clock = new THREE.Clock();
 
@@ -185,10 +392,10 @@ const tick = () => {
   //if (obj1) obj1.rotation.y = 0.5 * elapsedTime;
 
   // Update Orbital Controls
-  // controls.update()
+  //controls.update();
 
   // Render
-  renderer.render(scene, camera);
+  effect.render(scene, camera);
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick);
